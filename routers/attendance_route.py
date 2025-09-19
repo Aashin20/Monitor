8d19a7a37c25c4b78e9d271632f43dfec2843d03
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-09-19 23:13:27 +0530

    Create endpoint to create a new session

---
+++ routers/attendance_route.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel,Base64Bytes
+from fastapi import APIRouter
+from core.attendance import create_attendance_session
+from core.reg_attendance import register_attendance
+from core.attendance import get_attendance_summary, end_attendance_session
+from fastapi import  HTTPException
+from fastapi.responses import FileResponse
+from sqlalchemy import desc
+import csv
+import os
+import tempfile
+from datetime import datetime
+from utils.db import Database
+
+# Import your models and database session
+from models import (
+    AttendanceSession, AttendanceRecord, User, Course, 
+    StudentCourseEnrollment, AttendanceStatus
+)
+
+router = APIRouter()
+
+class AttendanceSessionCreate(BaseModel):
+    faculty_id: str
+    lat: float
+    lon: float
+    radius_meters: int
+    remarks: str = None
+
+class RegisterAttendance(BaseModel):
+    student_id: str
+    face: Base64Bytes
+    lat: float
+    lon: float
+
+@router.post("/session/create")
+async def create_session(details: AttendanceSessionCreate):
+    return create_attendance_session(
+        faculty_id=details.faculty_id,
+        lat=details.lat,
+        lon=details.lon,
+        radius_meters=details.radius_meters,
+        remarks=details.remarks
+    )
