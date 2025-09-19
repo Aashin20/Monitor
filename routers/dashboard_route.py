da8e317a0e344b4e2c9e9c407d1b07cd63b63021
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-09-19 23:12:35 +0530

    Create endpoint to fetch attendance history of a faculty

--- routers/dashboard_route.py
+++ routers/dashboard_route.py
@@ -6,3 +6,7 @@ router =  APIRouter()
 @router.get("/timetable/{student_reg_no}")
 async def fetch_timetable(student_reg_no: str):           
     return get_student_timetable(student_reg_no)
+
+@router.get("/faculty/{faculty_id}")
+async def fetch_faculty_history(faculty_id: str):   
+    return get_faculty_attendance_history(faculty_id)
